47f0957ccd6f86ab9251355f77c1a274ff6084d9
nilaayu345 <nilaayu890@gmail.com>
2021-10-14 09:40:15 +0700

    menampilkan data students

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -17,24 +17,24 @@
                     <table class="table table-responsive table-striped">
                         <thead>
                             <tr>
-                                <th>NIM</th>
-                                <th>Name</th>
-                                <th>Class</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($student as $s)
-                            <tr>
-                                <td>{{ $s->nim }}</td>
-                                <td>{{ $s->name }}</td>
-                                <td>{{ $s->class }}</td>
-                            </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                            <th>NIM</th>
+                            <th>Name</th>
+                            <th>Class</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($student as $s)
+                        <tr>
+                            <td>{{ $s->nim }}</td>
+                            <td>{{ $s->name }}</td>
+                            <td>{{ $s->class }}</td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
